aff04a451cedf8516460eab272a115795d1a1e9d
wahyu iqbal efriansyah 47 <wahyuiqbal91@gmail.com>
2019-06-07 20:20:46 +0700

    initial push

---
+++ guzzle.php
@@ -0,0 +1,36 @@
+<?php 
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+
+$response = $client->request('GET','http://omdbapi.com',[
+    'query' => [
+        'apikey' => 'f4f108f7',
+        's' => ''
+    ]
+]);
+
+$result = json_decode($response->getBody()->getContent(),true);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie</title>
+</head>
+<body>
+    <?php foreach ($result['Search'] as $movie) : ?>
+    <ul>
+        <li>Title : <?= $movie['Title'];?></li>
+        <li>Year : <?= $movie['Year'];?></li>
+        <li>
+            <img src="<?= $movie['Poster']; ?>" width="80">
+        </li>
+    </ul>
+    <?php endforeach; ?>
+</body>
+</html>
